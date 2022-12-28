c3b685ed98e77fae7e55cf48764cca11274cd211
Leandro Santiago <leandro.santiago@pebmed.com.br>
2022-12-28 19:56:25 -0300

    chore: create database structure

---
+++ postgres.sql
@@ -0,0 +1,42 @@
+CREATE TABLE "user" (
+  "id" BIGSERIAL NOT NULL PRIMARY KEY,
+  "timestamp" TIMESTAMP(3) NOT NULL,
+  "created" TIMESTAMP(3) NOT NULL,
+  "deleted" TIMESTAMP(3) NULL,
+  "email" VARCHAR(100) NOT NULL UNIQUE,
+  "password" TEXT NOT NULL
+);
+
+CREATE TABLE "customer" (
+  "id" BIGSERIAL NOT NULL PRIMARY KEY,
+  "timestamp" TIMESTAMP(3) NOT NULL,
+  "created" TIMESTAMP(3) NOT NULL,
+  "deleted" TIMESTAMP(3) NULL,
+  "user_id" BIGINT NOT NULL REFERENCES "user" ("id"),
+  "name" VARCHAR(100) NOT NULL,
+  "address" VARCHAR(200) NULL,
+  "phone" VARCHAR(20) NULL,
+  "email" VARCHAR(100) NULL,
+  "birthDate" DATE NULL,
+  "photoURL" TEXT NULL
+);
+
+CREATE TABLE "sale" (
+  "id" BIGSERIAL NOT NULL PRIMARY KEY,
+  "timestamp" TIMESTAMP(3) NOT NULL,
+  "created" TIMESTAMP(3) NOT NULL,
+  "deleted" TIMESTAMP(3) NULL,
+  "customer_id" BIGINT NOT NULL REFERENCES "customer" ("id"),
+  "status" VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE "sale_product" (
+  "id" BIGSERIAL NOT NULL PRIMARY KEY,
+  "sale_id" BIGINT NOT NULL REFERENCES "sale" ("id"),
+  "product_id" VARCHAR(100) NOT NULL,
+  "name" VARCHAR(100) NOT NULL,
+  "price" DECIMAL(10,2) NOT NULL,
+  "quantity" DECIMAL(10,2) NOT NULL,
+  "brand" VARCHAR(20) NOT NULL,
+  "tax" DECIMAL(10,2) NOT NULL
+);
